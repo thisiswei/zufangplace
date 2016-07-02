672ad3a417593e839f1b086e0684ee3222fbbc7d
thisiswei <99plus2@gmail.com>
2016-07-02 10:44:34 -0400

    adds

--- zufangplace/zufang/admin.py
+++ zufangplace/zufang/admin.py
@@ -6,13 +6,19 @@ from . import models
 class UserProfileAdmin(admin.ModelAdmin):
     pass
 
+
 class FangAdmin(admin.ModelAdmin):
     class Meta:
         ordering = (
             '-date_avaialable',
         )
 
-# Register your models here.
+
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+
 
 admin.site.register(models.UserProfile, UserProfileAdmin)
 admin.site.register(models.Fang, FangAdmin)
+admin.site.register(models.Address, AddressAdmin)
